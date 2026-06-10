332172892fe8d52472ddb6de1b3ab1fc6a12334d
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2026-06-10 00:08:35 -0400

    moving loops api key imports into the functions they're used in (i love testing in prod)

--- lib/loops.ts
+++ lib/loops.ts
@@ -1,22 +1,14 @@
-const LOOPS_API_KEY = process.env.LOOPS_API_KEY;
-const LOOPS_TRANSACTIONAL_SIGNIN_EMAIL_ID = process.env.LOOPS_TRANSACTIONAL_SIGNIN_EMAIL_ID;
-const LOOPS_TRANSACTIONAL_NOTIFICATION_EMAIL_ID = process.env.LOOPS_TRANSACTIONAL_NOTIFICATION_EMAIL_ID;
-const LOOPS_TRANSACTIONAL_PERSONALIZED_EMAIL_ID = process.env.LOOPS_TRANSACTIONAL_PERSONALIZED_EMAIL_ID;
-
-if (!LOOPS_TRANSACTIONAL_SIGNIN_EMAIL_ID) throw new Error("Please set LOOPS_TRANSACTIONAL_SIGNIN_EMAIL_ID");
-if (!LOOPS_TRANSACTIONAL_NOTIFICATION_EMAIL_ID) throw new Error("Please set LOOPS_TRANSACTIONAL_NOTIFICATION_EMAIL_ID");
-if (!LOOPS_TRANSACTIONAL_PERSONALIZED_EMAIL_ID) throw new Error("Please set LOOPS_TRANSACTIONAL_PERSONALIZED_EMAIL_ID");
-
 async function sendEmailWithLoops(
     transactionEmailId: string,
     targetEmail: string,
     emailParams: Record<string, string>,
 ) {
+    const apiKey = process.env.LOOPS_API_KEY;
     const response = await fetch("https://app.loops.so/api/v1/transactional", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            "Authorization": `Bearer ${LOOPS_API_KEY}`
+            "Authorization": `Bearer ${apiKey}`
         },
         body: JSON.stringify({
             transactionalId: transactionEmailId,
@@ -33,8 +25,10 @@ async function sendEmailWithLoops(
 }
 
 export async function sendAuthEmail(targetEmail: string, host: string, signin_url: string, datetime: string) {
+    const id = process.env.LOOPS_TRANSACTIONAL_SIGNIN_EMAIL_ID;
+    if (!id) throw new Error("Please set LOOPS_TRANSACTIONAL_SIGNIN_EMAIL_ID");
     await sendEmailWithLoops(
-        LOOPS_TRANSACTIONAL_SIGNIN_EMAIL_ID!,
+        id,
         targetEmail,
         {
             "hostname": host,
@@ -45,14 +39,14 @@ export async function sendAuthEmail(targetEmail: string, host: string, signin_ur
 }
 
 export async function sendNotificationEmail(targetEmail: string, name: string, date: string, content: string) {
-    await sendEmailWithLoops(
-        LOOPS_TRANSACTIONAL_NOTIFICATION_EMAIL_ID!,
-        targetEmail, { content, name, date });
+    const id = process.env.LOOPS_TRANSACTIONAL_NOTIFICATION_EMAIL_ID;
+    if (!id) throw new Error("Please set LOOPS_TRANSACTIONAL_NOTIFICATION_EMAIL_ID");
+    await sendEmailWithLoops(id, targetEmail, { content, name, date });
 }
 
 export async function sendPersonalizedEmail(targetEmail: string, name: string, reviewer: string, slackId: string, content: string) {
+    const id = process.env.LOOPS_TRANSACTIONAL_PERSONALIZED_EMAIL_ID;
+    if (!id) throw new Error("Please set LOOPS_TRANSACTIONAL_PERSONALIZED_EMAIL_ID");
     console.log(targetEmail, name, reviewer, slackId, content);
-    await sendEmailWithLoops(
-        LOOPS_TRANSACTIONAL_PERSONALIZED_EMAIL_ID!,
-        targetEmail, { name, content, reviewer, slackId });
+    await sendEmailWithLoops(id, targetEmail, { name, content, reviewer, slackId });
 }
